be589ac96e3f1c9adc4ca353512354367b3e67d6
Tess <tessdegregorio@gmail.com>
2017-11-21 11:34:29 +0100

    Edit is finally working

--- routes/profile.js
+++ routes/profile.js
@@ -11,6 +11,8 @@ router.get("/", ensureLoggedIn(), (req, res) => {
 router.get('/:id/edit', (req, res, next) => {
   User.findById(req.user._id, (err, user) => {
     console.log(req.user._id);
+    console.log(req.user);
+
       if (err) {
         console.log(err);
       }
@@ -20,12 +22,13 @@ router.get('/:id/edit', (req, res, next) => {
 
 router.post('/:id/edit', (req, res, next) => {
   let updates = {
-       username: req.body.username,
-       email: req.body.email,
-       password: req.body.password,
+       name: req.body.username,
+       email: req.body.email
        // pic_path: `../uploads/${req.file.filename}`,
        // pic_name: req.file.originalname
      };
+     console.log(updates)
+
      User.findByIdAndUpdate(req.user._id, updates, (err, result) => {
        if (err) {
          console.log(err);
