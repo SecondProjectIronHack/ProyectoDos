6983b9d6cc3e1d0513bf7639ceed5959af2f3c9e
Maria Ruiz <mariaruizestevez@gmail.com>
2017-11-24 11:29:58 +0100

    Deploy

--- public/javascripts/googlePlaces.js
+++ public/javascripts/googlePlaces.js
@@ -14,18 +14,14 @@ function initAutocomplete() {
           mapTypeId: 'roadmap'
         });
 
-        // Create the search box and link it to the UI element.
         var input = document.getElementById('pac-input');
         var searchBox = new google.maps.places.SearchBox(input);
 
-        // Bias the SearchBox results towards current map's viewport.
         map.addListener('bounds_changed', function() {
           searchBox.setBounds(map.getBounds());
         });
 
         var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
         searchBox.addListener('places_changed', function() {
           var places = searchBox.getPlaces();
 
@@ -33,13 +29,11 @@ function initAutocomplete() {
             return;
           }
 
-          // Clear out the old markers.
           markers.forEach(function(marker) {
             marker.setMap(null);
           });
           markers = [];
 
-          // For each place, get the icon, name and location.
           var bounds = new google.maps.LatLngBounds();
           places.forEach(function(place) {
             console.log(place);
@@ -97,11 +91,9 @@ function initAutocomplete() {
                     marker.addListener('click', function() {
                       infowindow.open(map, marker);
                     });
-                    console.log(place);
 
 
             if (place.geometry.viewport) {
-              // Only geocodes have viewport.
               bounds.union(place.geometry.viewport);
             } else {
               bounds.extend(place.geometry.location);
